9e6c10e14cdc957626a21101a17c4568f0a70674
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-06-20 20:51:03 +0300

    Example codes deleted.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navi from "./layouts/Navi/Navi";
+import UserDashboard from "./layouts/Dashboards/UserDashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navi />
+      <UserDashboard />
+    </div>
+  );
 }
 
 export default App;
