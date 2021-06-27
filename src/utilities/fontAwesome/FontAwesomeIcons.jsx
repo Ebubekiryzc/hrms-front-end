f81251abbf0f4429ad820438594fdde128b8d203
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2021-06-27 19:51:42 +0300

    Font awesome icon library created.

---
+++ src/utilities/fontAwesome/FontAwesomeIcons.jsx
@@ -0,0 +1,4 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser ,faLock, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faLock, faEnvelope);
