80caab6ef2a9d8c608fd7c3a9037b3380f20c535
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2021-06-27 19:50:42 +0300

    Refactorings...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Navi from "./layouts/Navi/Navi";
 import UserDashboard from "./layouts/Dashboards/UserDashboard";
-
 function App() {
   return (
     <div className="App">
