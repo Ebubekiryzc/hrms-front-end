b0dd3ac687f0c60cd03e333de9d5f483aef6afb2
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-06-30 23:50:06 +0300

    Added new icons.

--- src/utilities/fontAwesome/FontAwesomeIcons.jsx
+++ src/utilities/fontAwesome/FontAwesomeIcons.jsx
@@ -1,4 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUser ,faLock, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faUser ,faLock, faEnvelope, faBuilding, faGlobe, faPhone, faQuoteRight, faIdCard, faBirthdayCake} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUser, faLock, faEnvelope);
+library.add(faUser, faLock, faEnvelope, faBuilding, faGlobe, faPhone, faQuoteRight, faIdCard, faBirthdayCake);
