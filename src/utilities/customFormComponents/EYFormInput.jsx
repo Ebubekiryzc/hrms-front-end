393f01d6948d74b33b8da676c3b2021ce2dcc1f9
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-06-30 23:49:41 +0300

    Custom form component edited. (Meta module now active.)

--- src/utilities/customFormComponents/EYFormInput.jsx
+++ src/utilities/customFormComponents/EYFormInput.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { useField } from "formik";
+import { ErrorMessage, useField } from "formik";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function EYFormInput({ icon,...props }) {
   const [field,meta] = useField(props)
   return (
-    <div className="input-field">
+    <div className="input-field" >
       <div className="i">
       <FontAwesomeIcon icon={icon} fixedWidth />
       </div>
-      <input {...field} {...props}/>
+      <input className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`} autoComplete="false" {...field} {...props}/>
+      <div>
+      <ErrorMessage component="div" name={field.name}  className="error"/>
+      </div>
     </div>
   );
 }
