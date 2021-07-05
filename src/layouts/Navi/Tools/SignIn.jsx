5d4d9cd1ec58daa5f42f0f95bdfae088617343c3
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-07-05 19:04:19 +0300

    Sign out and Sign in components created.

--- src/layouts/Navi/Tools/SignIn.jsx
+++ src/layouts/Navi/Tools/SignIn.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { NavDropdown, Image } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function SignIn() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function SignIn({...props}) {
+  return (
+    <NavDropdown
+      title={
+        <>
+          <Image
+            className="avatar-img"
+            src={props.userPicture}
+            roundedCircle
+          />
+          <span className="user-name">{props.userName}</span>
+        </>
+      }
+    >
+      <NavDropdown.Item exact as={NavLink} to="/profile">&nbsp;
+        <FontAwesomeIcon icon="info" />
+        <span className="custom-dropdown-item-text">&nbsp;My Profile</span>
+      </NavDropdown.Item>
+      <NavDropdown.Item exact as={NavLink} to="/" onClick={props.logOut}>
+        <FontAwesomeIcon icon="sign-out-alt" />
+        <span className="custom-dropdown-item-text">Log out</span>
+      </NavDropdown.Item>
+    </NavDropdown>
+  );
 }
--- src/layouts/Navi/Tools/SignOut.jsx
+++ src/layouts/Navi/Tools/SignOut.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Button } from "react-bootstrap";
 
-export default function SignOut() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function SignOut({...props}) {
+  return (
+    <div onClick={props.logIn}>
+      <Button className="sign-up-btn">Register</Button>
+      <Button className="sign-in-btn">Sign In</Button>
+    </div>
+  );
 }
