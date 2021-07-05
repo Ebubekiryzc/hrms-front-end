8f04acd7bda824cc6f6c6a3f09f06c4e7a810b93
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-07-05 19:09:28 +0300

    Navigation bar component created.

--- src/layouts/Navi/Navi.jsx
+++ src/layouts/Navi/Navi.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import NaviItem from "./NaviItem";
+import SignIn from "./Tools/SignIn";
+import SignOut from "./Tools/SignOut";
+import "./Navi.css";
 
-export default function Navi() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Navi({ ...props }) {
+  const [navbarShrink, setNavbarShrink] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setIsAuthenticated(false);
+  }
+
+  function handleSignIn() {
+    setIsAuthenticated(true);
+  }
+
+  const changeBackground = () => {
+    if (window.scrollY >= 90) {
+      setNavbarShrink(true);
+    } else {
+      setNavbarShrink(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackground);
+  return (
+    <Navbar
+      expand="lg"
+      fixed="top"
+      className={navbarShrink ? "navbar-shrink" : ""}
+      collapseOnSelect
+    >
+      <Container>
+        <Navbar.Brand className="logo" href="/" style={{ color: "white" }}>
+          {props.logo}
+        </Navbar.Brand>
+        <Navbar.Toggle>
+          <FontAwesomeIcon icon="bars" />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="/">
+            <NaviItem pathAdr="/" displayName="Home" />
+            <NaviItem pathAdr="/about" displayName="About us" />
+            <NaviItem pathAdr="/jobadvertisements" displayName="Find a job" />
+            <NaviItem
+              pathAdr="/jobadvertisements/add"
+              displayName="Post a Job"
+            />
+            <Nav.Item className="sign-mode">
+              {isAuthenticated ? (
+                <SignIn logOut={handleSignOut} />
+              ) : (
+                <SignOut logIn={handleSignIn} />
+              )}
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- src/layouts/Navi/NaviItem.jsx
+++ src/layouts/Navi/NaviItem.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
-export default function NaviItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function NaviItem({ ...props }) {
+  return (
+    <Nav.Item>
+      <Nav.Link className={props.activeness} exact as={NavLink} to={props.pathAdr}>
+        {props.displayName}
+      </Nav.Link>
+    </Nav.Item>
+  );
 }
