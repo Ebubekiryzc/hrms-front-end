d2f8d5d4896647e73797794304ca2e1b07c1ad7a
Ebubekiryzc <ebubekiryazi@gmail.com>
2021-07-05 19:12:17 +0300

    Dashboard for users created. (Landing dash)

--- src/layouts/Dashboards/UserDashboard.jsx
+++ src/layouts/Dashboards/UserDashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router';
+import Home from "../../pages/Home/Home";
 import SignPage from '../../pages/SignPage/SignPage'
 
 export default function UserDashboard() {
     return (
         <div>
-            <SignPage></SignPage>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/sign" component={SignPage} />
         </div>
     )
 }
