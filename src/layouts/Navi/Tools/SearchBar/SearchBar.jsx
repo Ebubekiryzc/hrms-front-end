4920c28c7b489fbb16921f4250d91cb1629574f1
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-07-24 00:21:27 +0300

    Searchbar edited for new usage fields.

--- src/layouts/Navi/Tools/SearchBar/SearchBar.jsx
+++ src/layouts/Navi/Tools/SearchBar/SearchBar.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { InputGroup, FormControl, Button } from "react-bootstrap";
+import {
+  InputGroup,
+  FormControl,
+  Button,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import "./SearchBar.css";
 
 export default function SearchBar(props) {
   return (
     <InputGroup className={props.className}>
-      <FormControl
-        className={props.className}
-        placeholder={props.placeholder}
-        aria-label="Type something..."
-        aria-describedby="basic-addon2"
-      />
+      {props.inputAreas !== undefined ? (
+        props.inputAreas.map((prop) => (
+          <OverlayTrigger
+            key={props.className}
+            overlay={<Tooltip id={`tooltip-top`}>{prop.placeholder}</Tooltip>}
+          >
+            <FormControl
+              className={props.className}
+              placeholder={prop.placeholder}
+              aria-label="Type something..."
+              aria-describedby="basic-addon2"
+            />
+          </OverlayTrigger>
+        ))
+      ) : (
+          <FormControl
+            className={props.className}
+            placeholder={props.placeholder}
+            aria-label="Type something..."
+            aria-describedby="basic-addon2"
+          />
+      )}
       <Button className={props.className}>
         <FontAwesomeIcon icon="search" className={props.className} />
         &nbsp;&nbsp;{props.buttonText}
