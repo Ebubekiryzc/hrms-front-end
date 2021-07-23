4caca913eb9eff7816be4ea0f257f3d9b9177540
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-07-24 00:23:21 +0300

    Filter component edited, and will be updated.

--- src/layouts/Tools/AdvertisementFilter.jsx
+++ src/layouts/Tools/AdvertisementFilter.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Button, Collapse, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import JobService from "../../services/JobService";
-import EYCheckBox from "../../utilities/customComponents/EYCheckBox";
+import EYCheckBox from "../../utilities/customComponents/EYCheckBox/EYCheckBox";
+import SearchBar from "../Navi/Tools/SearchBar/SearchBar";
+import "./AdvertisementFilter.css";
 
 export default function AdvertisementFilter() {
   const [checkStates, setCheckStates] = useState([]);
   const [jobs, setJobs] = useState([]);
+  const [open, setOpen] = useState(false);
 
   const handleOnChange = (position) => {
     const updatedCheckedState = checkStates.map((item, index) =>
@@ -13,6 +18,36 @@ export default function AdvertisementFilter() {
     );
     setCheckStates(updatedCheckedState);
   };
+
+  const filterMenu = (
+    <>
+      <div className="border-bottom pb-2 ml-2">
+        <h4 className="filter-text">Filters</h4>
+      </div>
+      <div className="position-menu py-2 border-bottom ml-3">
+        <h6>Position</h6>
+        <FontAwesomeIcon icon="minus" className="decoration-icon" />
+        <SearchBar
+          className="search"
+          placeholder="Search for a Position..."
+          buttonText="Find"
+        />
+        <Form className="scrollable-list">
+          {jobs.map((job, index) => (
+            <EYCheckBox
+              key={job.id}
+              id={`custom-checkbox-${index}`}
+              isChecked={checkStates[index]}
+              labelText={job.name}
+              onChangeFunction={() => {
+                handleOnChange(index);
+              }}
+            />
+          ))}
+        </Form>
+      </div>
+    </>
+  );
 
   useEffect(() => {
     let jobService = new JobService();
@@ -26,20 +61,25 @@ export default function AdvertisementFilter() {
 
   return (
     <div className="advertisement-filter-overlay section-padding">
-      {jobs.map((job, index) => (
-        //TODO: Eğer okeyse gözüksün
-
-        <EYCheckBox
-          key={job.id}
-          id={`custom-checkbox-${index}`}
-          isChecked={checkStates[index]}
-          labelText={job.name}
-          onChangeFunction={() => {
-            handleOnChange(index);
-            checkStates[index] ? setJobs() : setJobs();
-          }}
-        />
-      ))}
+      <div className="filter-button-div">
+        <Button
+          className="filter-button"
+          onClick={() => setOpen(!open)}
+          aria-controls="mobile-filter"
+          aria-expanded={open}
+        >
+          Filters
+          <FontAwesomeIcon icon="filter" className="filter-icon" />
+        </Button>
+      </div>
+      <Collapse in={open}>
+        <div className="filter" id="mobile-filter">
+          {filterMenu}
+        </div>
+      </Collapse>
+      <section className="filter" id="sidebar-filter">
+        {filterMenu}
+      </section>
     </div>
   );
 }
